c187f7959f969ec3f12e9e48bff386d64b50d2f6
Tawheed tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-05-09 08:03:13 +0530

    added file manipulation program

---
+++ basic system calls/file manipulation sys calls/6_file_program.c
@@ -0,0 +1,20 @@
+//program to take input from STDIN and store in output.txt
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<string.h>
+
+
+int main(){
+    int fd;
+    char buf[2000];
+    char buf2[] = "Write the text you want to get stored in file:\n";
+    fd = open("output.txt", O_RDWR | O_CREAT, 0644);
+
+    write(1, buf2, strlen(buf2));//write on STDOUT
+    read(0, buf, 2000); //read from STDIN
+    write(fd, buf, strlen(buf)); //write to file
+    return 0;
+}
